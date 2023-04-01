3daacf4e36806e42677779541df105176792dfb9
pedrojamiss <pedrojamiss@gmail.com>
2023-03-31 19:15:27 -0700

    fist commit

---
+++ script/style-switcher.js
@@ -0,0 +1,23 @@
+const styleSwitcherToggler = document.querySelector(".style-switcher-toggler");
+styleSwitcherToggler.addEventListener("click", ()=>{
+    document.querySelector(styleSwitcher).classList.toggle("open");
+
+})
+ window.addEventListener("scroll", ()=>{
+    if(document.querySelector(".style-switcher").classList.contains("open")){
+        document.querySelector(".style-switcher").classList.remove("open");
+    }
+ })
+
+ const alternateStyle = document.querySelectorAll(".alternate-style");
+ function setActiveStyle(color){
+    localStorage.setItem("color",color);
+    changeColor();
+ }
+ function changeColor(){
+    alternateStyle.forEach(style) => {
+        if(localStorage.getItem("color")===style.getAttribute("title")){
+            style.removeAttribute("disabled");
+        }
+    }
+ }
